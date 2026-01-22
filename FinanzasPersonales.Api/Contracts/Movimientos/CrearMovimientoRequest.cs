1ce7dc5f52bd0f80d2d619bfc459e3cc47c2c634
Matias <88720232+Matubil@users.noreply.github.com>
2026-01-21 22:04:21 -0300

    Add/ Se agrega Contract Crear Movimiento request
    Se agrega como contract pero es un Dto de controlador para respuesta

---
+++ FinanzasPersonales.Api/Contracts/Movimientos/CrearMovimientoRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace FinanzasPersonales.Contracts.Movimientos;
+
+public class CrearMovimientoRequest
+{
+    public Guid UsuarioId { get; init; }
+    public Guid CuentaId { get; init; }
+    public Guid CategoriaId { get; init; }
+
+    public decimal Monto { get; init; }
+    public DateOnly Fecha { get; init; }
+    public string? Descripcion { get; init; }
+
+    public bool EsIngreso { get; init; }
+
+    public int CantidadCuotas { get; init; } = 1;
+    public Guid? TarjetaId { get; init; }
+}
