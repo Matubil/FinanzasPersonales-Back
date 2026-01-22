1f5f5a05a44d4e337c3d96ab3711d196abe9a668
Matias <88720232+Matubil@users.noreply.github.com>
2026-01-21 22:01:42 -0300

    Add/ Se agrega la conexion factory

---
+++ FinanzasPersonales.Infrastructure/Data/DbConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Data;
+
+namespace FinanzasPersonales.Infrastructure.Data;
+
+public class DbConnectionFactory
+{
+    private readonly IConfiguration _configuration;
+
+    public DbConnectionFactory(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public IDbConnection Create()
+    {
+        return new NpgsqlConnection(
+            _configuration.GetConnectionString("DefaultConnection")
+        );
+    }
+}
